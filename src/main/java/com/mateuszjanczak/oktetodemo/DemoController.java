256161bc9cc65b6c83c548df52ce443b4b3fabaa
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2022-09-10 18:16:42 +0200

    Add load balancer and logger

--- src/main/java/com/mateuszjanczak/oktetodemo/DemoController.java
+++ src/main/java/com/mateuszjanczak/oktetodemo/DemoController.java
@@ -1,16 +1,23 @@
 package com.mateuszjanczak.oktetodemo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class DemoController {
 
+    Logger logger = LogManager.getLogger(DemoController.class);
+
     @GetMapping("/test")
-    public ResponseEntity<String> demo() {
+    public ResponseEntity<String> demo(HttpServletRequest request) {
+        logger.info("Request: " + request.getRemoteAddr());
         return new ResponseEntity<>("Hello okteto", OK);
     }
 }
